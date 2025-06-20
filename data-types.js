0dbc39a177d663a98369573641303f9a59f4281f
Naim Uddin <naimuddin374@gmail.com>
2025-06-20 19:37:22 +0600

    class

---
+++ data-types.js
@@ -0,0 +1,90 @@
+/**
+ * In JavaScript has 8 datatypes
+ * 1. Number
+ * 2. String
+ * 3. Boolean
+ * 4. Undefined
+ * 5. Null
+ * 7. BigInt
+ * 8. Object
+ */
+
+/**
+ * In computer science two types of language
+ * 1. High level language
+ * 2. Low level language
+ */
+// Java
+// var name: string = "John"; // String
+// var email: string = "john@gmail.com"; // String
+// var age: number = 30; // Number
+
+// JavaScript
+// var name = "John"; // String
+// var email = "john@gmail.com";
+// var age = 30; // Number
+
+// console.log("Name:", typeof name);
+// console.log("email:", typeof email);
+// console.log("Age:", typeof age);
+
+// Example: Number and String
+// let age = 30; // Number
+// let ageString = "30"; // String
+// let name = "mr. john"; // String
+
+// // age = age + 10; // Number
+
+// ageString = "30/10" + " Tamim";
+
+// console.log(name.length); // Output: 2
+// console.log(name.toUpperCase());
+// console.log(age);
+// console.log(ageString);
+
+// Example: Boolean
+// Always give yes or no answer
+// Yes = true and No = false
+
+// const rezon = true; // Boolean
+// const tamim = false; // Boolean
+// console.log(rezon, tamim);
+
+// Example: Undefined
+// Property = Value
+// let name; // Undefined
+// let age; // Undefined
+
+// console.log(name, age);
+
+// Example: Null
+// Null is a special value that represents the intentional absence of any object value.
+// let name = null; // Null
+// let age = null; // Null
+
+// console.log(name, age);
+
+// Example: BigInt
+// BigInt is a built-in object that provides a way to represent whole numbers larger than 2^53 - 1.
+// let bigNumber = 1234567890123456789012345678901234567890n; // BigInt
+// name, email, phone, uniqueId
+
+// Example: Object
+// Objects are collections of key-value pairs.
+
+let person = {
+  name: "Tamim",
+  age: 35,
+  email: "tamim@gmail.com",
+  isMarried: false,
+  gender: "Male",
+  height: 5.9,
+  address: {
+    city: "Dhaka",
+    country: "Bangladesh",
+    zipCode: "1212",
+  },
+};
+console.log(person);
+
+// Important Data-Types: String, Number, Object, Boolean
