cdbf33cbab5c459397cf40427e0860d347638904
Naim Uddin <naimuddin374@gmail.com>
2025-07-14 23:31:25 +0600

    Array

---
+++ array.js
@@ -0,0 +1,64 @@
+// const arr = Array(10, 20, 30); // before 2015
+// const tamim = [10, 20, 30]; // after 2015
+
+// console.log(arr);
+// console.log(tamim);
+
+// const studentList = [
+//   {
+//     id: 1,
+//     name: "Tamim",
+//     age: 32,
+//   },
+//   {
+//     id: 2,
+//     name: "Rezon",
+//     age: 23,
+//   },
+//   {
+//     id: 3,
+//     name: "Sujon",
+//     age: 22,
+//   },
+//   [10, 20, 30, [1, 2, 3, 4, [33, 44, 55, 66, [100, 200, 33]]]],
+//   30,
+//   "ABC",
+// ];
+// console.log(studentList[3]);
+
+const todayAssignment = [
+  {
+    name: "Sohag",
+    point: 8,
+  },
+  {
+    name: "Tamim",
+    point: 5,
+  },
+  {
+    name: "Rezon",
+    point: 0,
+  },
+  {
+    name: "Biddut",
+    point: 0,
+  },
+  {
+    name: "Sujon",
+    point: 0,
+  },
+  {
+    name: "Sowkot",
+    point: 0,
+  },
+  {
+    name: "Rashed",
+    point: 0,
+  },
+  {
+    name: "Alamin",
+    point: 0,
+  },
+];
+console.log(todayAssignment);
+// console.log(todayAssignment[3]);
--- loops/nested-loop.js
+++ loops/nested-loop.js
@@ -38,13 +38,13 @@
  *********
  **********
  */
-for (let i = 0; i < 10; i++) {
-  let str = "";
-  for (let j = 0; j <= i; j++) {
-    str += "*";
-  }
-  console.log(str);
-}
+// for (let i = 0; i < 10; i++) {
+//   let str = "";
+//   for (let j = 0; j <= i; j++) {
+//     str += "*";
+//   }
+//   console.log(str);
+// }
 
 /*
 ============Assignment 1=============
@@ -66,6 +66,76 @@ for (let i = 0; i < 10; i++) {
  
 
 ============Assignment 3=============
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+*/
+
+// for (let i = 5; i >= 0; i--) {
+//   let str = "";
+//   for (let j = 0; j <= i; j++) {
+//     str += "*";
+//   }
+//   console.log(str);
+// }
+
+/***
+ ******
+ *****
+ ****
+ ***
+ **
+ *
+ ***/
+
+// for (let i = 1; i <= 5; i++) {
+//   let row = "";
+//   for (let j = 1; j <= 5; j++) {
+//     row += i + " ";
+//     // i=1 => 1 1 1 1 1
+//     // i=2 => 2 2 2 2 2
+//     // i=3 => 3 3 3 3 3
+//   }
+//   console.log(row);
+// }
+
+/**
+1 1 1 1 1 
+2 2 2 2 2 
+3 3 3 3 3 
+4 4 4 4 4 
+5 5 5 5 5 
+ */
+
+// for (let i = 1; i <= 5; i++) {
+//   let row = "";
+//   for (let j = 1; j <= i; j++) {
+//     row += i + " ";
+//   }
+//   console.log(row);
+// }
+
+/**
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+ */
+
+let num = 1;
+for (let i = 1; i <= 5; i++) {
+  let tamim = "";
+  for (let j = 1; j <= i; j++) {
+    tamim += num + " ";
+    num++;
+  }
+  console.log(tamim);
+}
+
+/**
 1
 2 3
 4 5 6
