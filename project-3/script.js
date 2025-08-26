14370fd403737ca5846ae2253a0b3279463161ce
Naim Uddin <naimuddin374@gmail.com>
2025-08-26 22:50:03 +0600

    class 30

---
+++ project-3/script.js
@@ -0,0 +1,50 @@
+/**
+ * In javascript many ways to fetch data from api
+ * 1. Fetch API
+ * 2. Axios
+ * 3. XMLHttpRequest
+ *
+ * First fetch data from jsonplaceholder.typicode.com
+ * Then display the posts on the page
+ * Add pagination logic over here (optional)
+ */
+// "https://jsonplaceholder.typicode.com/posts"
+
+const imageUrl = `https://t4.ftcdn.net/jpg/00/65/48/25/360_F_65482539_C0ZozE5gUjCafz7Xq98WB4dW6LAhqKfs.jpg`;
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Posts data:", data);
+    const mainDiv = document.querySelector("main");
+    data.forEach((post) => {
+      const postWrapper = document.createElement("div");
+      postWrapper.classList.add("post-wrapper");
+      postWrapper.innerHTML = `
+          <h2>ID: ${post.id} - ${post.title}</h2>
+          <img src="${imageUrl}" alt="Post Image">
+          <p>${post.body}</p>
+          <a href="#">Read More</a>
+        `;
+      mainDiv.appendChild(postWrapper);
+    });
+  })
+  .catch((error) => console.error("Error fetching posts:", error));
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log("Posts data:", data);
+//     const postsContainer = document.querySelector("main");
+//     data.forEach((post) => {
+//       const postWrapper = document.createElement("div");
+//       postWrapper.classList.add("post-wrapper");
+//       postWrapper.innerHTML = `
+//         <h2>${post.title}</h2>
+//         <p>${post.body}</p>
+//         <a href="#">Read More</a>
+//       `;
+//       postsContainer.appendChild(postWrapper);
+//     });
+//   })
+//   .catch((error) => console.error("Error fetching posts:", error));
